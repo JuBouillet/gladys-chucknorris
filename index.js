b7b758a164d7f7367e007694a6b3399ec8b06a19
Julien Bouillet <jbouillet@eliade.fr>
2018-12-01 16:16:42 +0100

    Nouveau test

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var facts = require('./lib/facts.js');
-var command = require('./lib/facts.command.js');
-var install = require('./lib/install.js');
-var uninstall = require('./lib/uninstall.js');
-
 module.exports = function (sails) {
+	var facts = require('./lib/facts.js');
+	var command = require('./lib/facts.command.js');
+	var install = require('./lib/install.js');
+	var uninstall = require('./lib/uninstall.js');
 	return {
 		facts: facts,
 		// reboot: reboot,
