1caf4100eb0238516b3d264223c8344079b47416
Julien Bouillet <jbouillet@eliade.fr>
2018-12-02 19:19:06 +0100

    Suppression de log inutile

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = function (sails) {
 	var command = require('./lib/facts.command.js');
 	var install = require('./lib/install.js');
 	var uninstall = require('./lib/uninstall.js');
-	console.log("Test CNFACTS");
 	return {
 		facts: facts,
 		command: command,
