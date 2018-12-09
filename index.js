75ecaf8763b9f2d3ec4b978883fb4ec5440133f1
Julien Bouillet <jbouillet@eliade.fr>
2018-12-09 10:21:03 +0100

    Utilisation de la méthode setup pour installer les éléments du module

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ module.exports = function (sails) {
 	var facts = require('./lib/facts.js');
 	var command = require('./lib/facts.command.js');
 	var install = require('./lib/install.js');
+	var setup = require('./lib/setup.js');
 	var uninstall = require('./lib/uninstall.js');
 	return {
 		facts: facts,
 		command: command,
 		install: install,
 		uninstall: uninstall,
+		setup: setup
 	};
 };
